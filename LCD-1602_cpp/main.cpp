b3aab055af1c220f7b6acf245469db5838a144c3
Ovidiu22 <75970114+Ovidiu22@users.noreply.github.com>
2023-03-09 18:18:19 +0100

    Function implementation

---
+++ LCD-1602_cpp/main.cpp
@@ -0,0 +1,26 @@
+/*******************************/
+/*** Created by Ovidiu Sabau ***/
+/***	9th March 2023		 ***/
+/*******************************/
+
+/*----------------- Include -------------------*/
+#include "LCD.h"
+
+/*-------------- Main function ----------------*/
+int main(void)
+{
+	uint8_t temperature = 16; //C
+	uint8_t rel_humidity = 42; //%
+	
+	initLCD();
+	//updateLCDScreen(row on LCD, "Text 1", number, "Text 2");
+	updateLCDScreen(1, "Hello there!", NONE, "NONE"); _delay_ms(1000);
+	updateLCDScreen(1, "CLEAR", NONE, "NONE");
+	
+	_delay_ms(1000);
+	
+	updateLCDScreen(1, "Temperature: ", temperature, "C");
+	updateLCDScreen(2, "Humidity: ", rel_humidity, "%");
+
+	return 0;
+}
